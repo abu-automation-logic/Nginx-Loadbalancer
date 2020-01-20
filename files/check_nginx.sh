aa908824e44e7ac0c434e1736aae5b51d195f634
abubakar-malik <53573520+abubakar-malik@users.noreply.github.com>
2020-01-20 14:14:19 +0000

    Add files via upload

---
+++ files/check_nginx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+result=$(sudo netstat -tulpn | grep :80 | grep nginx);
+
+if [ ! -z "$result" ]; then
+  echo "Nginx running on port 80";
+else
+  echo "Nginx is NOT running on port 80";
+fi
